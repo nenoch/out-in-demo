fee7e21ee7b54ccafc6f5c3240b7bab7d23bee7a
Irene Canuti <irene.canuti@gmail.com>
2017-10-07 13:11:55 +0100

    working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Output Input Demo!';
+  checked = false;
+  sets = [
+      {
+          name: "blue",
+          words: [
+              {
+                  wname: "sky",
+                  answers: []
+              },
+              {
+                  wname: "peacock",
+                  answers: []
+              }
+          ]
+      },
+      {
+          name: "red",
+          words: [
+              {
+                  wname: "cherry",
+                  answers: []
+              },
+              {
+                  wname: "blood",
+                  answers: []
+              }
+          ]
+      }
+  ];
+
+  consoleLog(value){
+    console.log(value);
+  }
+
+  handleCheckbox(array, answer){
+    let index = array.indexOf(answer);
+    if (index !== -1 ){
+      array.splice(index, 1)
+    } else {
+      array.push(answer);
+      console.log(this.sets);
+    }
+  }
 }
